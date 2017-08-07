ef7fa63441474f7f85b3f2099748634caf40b2a7
kinderyj <kinderyj@qq.com>
2017-08-07 19:33:18 +0800

    demoLogging.py

---
+++ demo/demoLogging.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#日志级别大小关系为：CRITICAL > ERROR > WARNING > INFO > DEBUG > NOTSET，当然也可以自己定义日志级别
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='myapp.log',
+                filemode='w')
+    
+logging.debug('This is debug message')
+logging.info('This is info message')
+logging.warning('This is warning message')
